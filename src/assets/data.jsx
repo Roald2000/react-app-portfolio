d81cc25736f534bc937275d4644be57f2718c6f4
Roald Dela Cruz <roalddelacruz2000@gmail.com>
2023-08-18 01:34:18 +0800

    minor updates

--- src/assets/data.jsx
+++ src/assets/data.jsx
@@ -1,118 +1,189 @@
 import {
-    AiOutlineLinkedin,
-    AiOutlineMail,
-    AiOutlinePhone,
+  AiOutlineLinkedin,
+  AiOutlineMail,
+  AiOutlinePhone,
 } from "react-icons/ai";
 
-import { SiMysql, SiExpress, SiTailwindcss, SiJavascript, SiNodedotjs, SiCss3, SiHtml5, SiPrisma, SiPhp, SiNodemon, SiDaisyui, SiReact, SiCsharp, SiMicrosoftaccess, SiVisualstudio, SiUikit, SiSqlite } from "react-icons/si";
+import {
+  SiMysql,
+  SiExpress,
+  SiTailwindcss,
+  SiJavascript,
+  SiNodedotjs,
+  SiCss3,
+  SiHtml5,
+  SiPrisma,
+  SiPhp,
+  SiNodemon,
+  SiDaisyui,
+  SiReact, 
+} from "react-icons/si";
 
 export const info = [
-    {
-        name: "Gmail",
-        content: "roalddelacruz2000@gmail.com",
-        icon: <AiOutlineMail size={24} />,
-    },
-    {
-        name: "Phone",
-        content: <a href="tel:+639925744271">
-            <span className="p-1 rounded bg-black text-white mr-1">+63</span>
-            <p className="inline">992-574-4271</p>
-        </a>,
-        icon: <AiOutlinePhone size={24} />,
-    },
-    {
-        name: "LinkedIn",
-        content: <a href="https://linkedin.com/in/roald-dela-cruz-6a180127a" className="" target="_blank" rel="noreferrer">View Profile</a>,
-        link: "https://linkedin.com/in/roald-dela-cruz-6a180127a",
-        icon: <AiOutlineLinkedin size={24} />,
-    },
+  {
+    name: "Gmail",
+    content: "roalddelacruz2000@gmail.com",
+    icon: <AiOutlineMail size={24} />,
+  },
+  {
+    name: "Phone",
+    content: (
+      <a href="tel:+639925744271">
+        <span className="p-1 rounded bg-black text-white mr-1">+63</span>
+        <p className="inline">992-574-4271</p>
+      </a>
+    ),
+    icon: <AiOutlinePhone size={24} />,
+  },
+  {
+    name: "LinkedIn",
+    content: (
+      <a
+        href="https://linkedin.com/in/roald-dela-cruz-6a180127a"
+        className=""
+        target="_blank"
+        rel="noreferrer"
+      >
+        Make a connection
+      </a>
+    ),
+    link: "https://linkedin.com/in/roald-dela-cruz-6a180127a",
+    icon: <AiOutlineLinkedin size={24} />,
+  },
 ];
-
-
-
 
 export const skills = [
-    {
-        name: 'Web Development',
-        tech: {
-            lang: [
-                { name: 'HTML', icon: <SiHtml5 size={36} /> },
-                { name: 'CSS', icon: <SiCss3 size={36} /> },
-                { name: 'JavaScript', icon: <SiJavascript size={36} /> },
-                { name: 'PHP', icon: <SiPhp size={36} /> },
-                { name: 'MySQL', icon: <SiMysql size={36} /> },
-            ],
-            front_stack: [
-                { name: 'React', icon: <SiReact size={36} /> },
-                { name: 'TailwindCSS', icon: <SiTailwindcss size={36} /> },
-                { name: 'DaisyUI', icon: <SiDaisyui size={36} /> },
-            ],
-            back_stack: [
-                { name: 'NodeJs', icon: <SiNodedotjs size={36} /> },
-                { name: 'ExpressJs', icon: <SiExpress size={36} /> },
-                { name: 'Nodemon', icon: <SiNodemon size={36} /> },
-                { name: 'Prisma', icon: <SiPrisma size={36} /> },
-                { name: 'MySQL - DB', icon: <SiMysql size={36} /> },
-            ],
-        }
+  {
+    name: "Web Development",
+    tech: {
+      lang: [
+        { name: "HTML", icon: <SiHtml5 size={36} /> },
+        { name: "CSS", icon: <SiCss3 size={36} /> },
+        { name: "JavaScript", icon: <SiJavascript size={36} /> },
+        { name: "PHP", icon: <SiPhp size={36} /> },
+      ],
+      front_stack: [
+        { name: "React", icon: <SiReact size={36} /> },
+        { name: "TailwindCSS", icon: <SiTailwindcss size={36} /> },
+        { name: "DaisyUI", icon: <SiDaisyui size={36} /> },
+      ],
+      back_stack: [
+        { name: "NodeJs", icon: <SiNodedotjs size={36} /> },
+        { name: "ExpressJs", icon: <SiExpress size={36} /> },
+        { name: "Nodemon", icon: <SiNodemon size={36} /> },
+        { name: "Prisma", icon: <SiPrisma size={36} /> },
+        { name: "MySQL - DB", icon: <SiMysql size={36} /> },
+      ],
     },
-    // {
-    //     name: 'Application Development',
-    //     tech: {
-    //         lang: [
-    //             { name: 'C#', icon: <SiCsharp size={36} /> },
-    //             { name: 'Visual Basic', icon: <div>Visual Basic</div> },
-    //         ],
-    //         front_stack: [
-    //             { name: 'C# GUI', icon: <SiCsharp size={36} /> },
-    //         ],
-    //         back_stack: [
-    //             { name: 'MySQL', icon: <SiMysql size={36} /> },
-    //             { name: 'MSAccess', icon: <SiMicrosoftaccess size={36} /> },
-    //             { name: 'SQLite', icon: <SiSqlite size={36} /> },
-    //         ],
-    //     }
-    // }
-]
-
-import { BiLogoTailwindCss, BiLogoReact } from 'react-icons/bi';
-import { FaNodeJs } from 'react-icons/fa';
-
-export const projects = [
-    {
-        name: "Static React Task/Todo App",
-        description: "This is a simplistic demo of a Todo app without a backend implementation. However, a backend will soon be included. So it can be called a Full-Stack Application that includes both Frontend and Backend development",
-        link: "https://roald2000.github.io/static-react-task-app",
-        stack: [{ tech: <BiLogoTailwindCss key={'BiLogoTailwindCss'} size={32} />, techName: 'TailwindCSS' }, { tech: <BiLogoReact key={'BiLogoReact'} size={32} />, techName: 'React' }]
-    },
-    {
-        name: "Simple Todo/Task REST API",
-        description: "This application is built using MySQL as the database for data storage and Prisma as the ORM for database models, with JSON Web Token (JWT) authentication implemented for security. It also provides users with the ability to read, create, update, and  their own data, ensuring a personalized experience.",
-        link: "https://github.com/Roald2000/task-management-rest-prisma-api",
-        stack: [{ tech: <FaNodeJs key={'FaNodeJs'} size={32} />, techName: 'NodeJs' }, { tech: <SiExpress key={'SiExpress'} size={32} />, techName: 'ExpressJs' }, { tech: <SiMysql key={'SiMysql'} size={32} />, techName: 'MySql' }, { techName: 'Prisma', tech: <SiPrisma key={'SiPrisma'} size={32} /> }]
-    },
-    {
-        name: "FullStack Logbook App",
-        description: "A logbook application where an employee/user times in and times out",
-        link: "https://github.com/Roald2000/LogBook",
-        stack: [
-            { tech: <SiReact size={32} />, techName: 'Reach' },
-            { tech: <SiTailwindcss size={32} />, techName: 'TailwindCss' },
-            { tech: <SiPhp size={32} />, techName: 'PHP - REST API' },
-            { tech: <SiMysql size={32} />, techName: 'MySQL - DB' },
-        ]
-    },
-    {
-        name: "PHP RESTful API",
-        description: "This app contains the backend code for an API. The API provides endpoints for retrieving information from and adding information to a database. The endpoints are related to a system that logs the time when employees of a company clock in and out.",
-        link: "https://github.com/Roald2000/php_api_with_bramus_router",
-        stack: [
-            { tech: <SiPhp size={32} />, techName: 'PHP - REST API' },
-            { tech: <SiMysql size={32} />, techName: 'MySQL - DB' },
-            { tech: <a href='https://packagist.org/packages/symfony/dotenv' target='_blank' rel="noreferrer" className='text-xs link link-primary'>symfony/dotenv</a>, techName: 'Accessing environment variables' },
-            { tech: <a href='https://github.com/bramus/router' target='_blank' rel="noreferrer" className='text-xs link link-primary'>bramus/router</a>, techName: 'Routing API endpoints' },
-        ]
-    },
+  },
+  // {
+  //     name: 'Application Development',
+  //     tech: {
+  //         lang: [
+  //             { name: 'C#', icon: <SiCsharp size={36} /> },
+  //             { name: 'Visual Basic', icon: <div>Visual Basic</div> },
+  //         ],
+  //         front_stack: [
+  //             { name: 'C# GUI', icon: <SiCsharp size={36} /> },
+  //         ],
+  //         back_stack: [
+  //             { name: 'MySQL', icon: <SiMysql size={36} /> },
+  //             { name: 'MSAccess', icon: <SiMicrosoftaccess size={36} /> },
+  //             { name: 'SQLite', icon: <SiSqlite size={36} /> },
+  //         ],
+  //     }
+  // }
 ];
 
+export const personal_skills = [
+  { name: "Task Management" },
+  { name: "Adaptability" },
+  { name: "Flexibility" },
+  { name: "Detail Oriented" },
+  { name: "System OS Installation | Re-format" },
+];
 
+import { BiLogoTailwindCss, BiLogoReact } from "react-icons/bi";
+import { FaNodeJs } from "react-icons/fa";
+
+export const projects = [
+  {
+    name: "Static React Task/Todo App",
+    description:
+      "This is a simplistic demo of a Todo app without a backend implementation. However, a backend will soon be included. So it can be called a Full-Stack Application that includes both Frontend and Backend development",
+    link: "https://roald2000.github.io/static-react-task-app",
+    stack: [
+      {
+        tech: <BiLogoTailwindCss key={"BiLogoTailwindCss"} size={32} />,
+        techName: "TailwindCSS",
+      },
+      {
+        tech: <BiLogoReact key={"BiLogoReact"} size={32} />,
+        techName: "React",
+      },
+    ],
+  },
+  {
+    name: "Simple Todo/Task REST API",
+    description:
+      "This application is built using MySQL as the database for data storage and Prisma as the ORM for database models, with JSON Web Token (JWT) authentication implemented for security. It also provides users with the ability to read, create, update, and  their own data, ensuring a personalized experience.",
+    link: "https://github.com/Roald2000/task-management-rest-prisma-api",
+    stack: [
+      { tech: <FaNodeJs key={"FaNodeJs"} size={32} />, techName: "NodeJs" },
+      {
+        tech: <SiExpress key={"SiExpress"} size={32} />,
+        techName: "ExpressJs",
+      },
+      { tech: <SiMysql key={"SiMysql"} size={32} />, techName: "MySql" },
+      { techName: "Prisma", tech: <SiPrisma key={"SiPrisma"} size={32} /> },
+    ],
+  },
+  {
+    name: "FullStack Logbook App",
+    description:
+      "A logbook application where an employee/user times in and times out",
+    link: "https://github.com/Roald2000/LogBook",
+    stack: [
+      { tech: <SiReact size={32} />, techName: "Reach" },
+      { tech: <SiTailwindcss size={32} />, techName: "TailwindCss" },
+      { tech: <SiPhp size={32} />, techName: "PHP - REST API" },
+      { tech: <SiMysql size={32} />, techName: "MySQL - DB" },
+    ],
+  },
+  {
+    name: "PHP RESTful API",
+    description:
+      "This app contains the backend code for an API. The API provides endpoints for retrieving information from and adding information to a database. The endpoints are related to a system that logs the time when employees of a company clock in and out.",
+    link: "https://github.com/Roald2000/php_api_with_bramus_router",
+    stack: [
+      { tech: <SiPhp size={32} />, techName: "PHP - REST API" },
+      { tech: <SiMysql size={32} />, techName: "MySQL - DB" },
+      {
+        tech: (
+          <a
+            href="https://packagist.org/packages/symfony/dotenv"
+            target="_blank"
+            rel="noreferrer"
+            className="text-xs link link-primary"
+          >
+            symfony/dotenv
+          </a>
+        ),
+        techName: "Accessing environment variables",
+      },
+      {
+        tech: (
+          <a
+            href="https://github.com/bramus/router"
+            target="_blank"
+            rel="noreferrer"
+            className="text-xs link link-primary"
+          >
+            bramus/router
+          </a>
+        ),
+        techName: "Routing API endpoints",
+      },
+    ],
+  },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,173 +1,244 @@
-
-
 import {
+  FaArrowRight,
   FaCloudRain,
   FaDiagramProject,
   FaGear,
   FaGithub,
-  FaPeopleGroup,
+  FaPeopleRoof,
   FaPooStorm,
+  FaPerson,
 } from "react-icons/fa6";
 
-import {
-  BiPen
-} from "react-icons/bi";
+import { BiPen } from "react-icons/bi";
 
-import { info, skills } from '../assets/data';
-import { FaSchool, } from "react-icons/fa";
-
-
-
+import { info, personal_skills, skills } from "../assets/data";
+import { FaMailBulk, FaSchool } from "react-icons/fa";
 
 export default function Home() {
   return (
     <>
+      {/* Intro */}
       <section className="w-full">
         <h2 className="my-2 text-2xl sm:text-4xl">Digital Porfolio | Resume</h2>
-      </section>
-      <section className="w-full pt-2">
-        <h4 className="text-xl flex flex-col items-start gap-1 flex-wrap">
+        <h4 className="text-xl flex items-start gap-1 flex-wrap">
           <span className="text-xs sm:text-lg font-bold">Roald Dela Cruz</span>
-          <span className="text-xs sm:text-md p-1 rounded kbd ">
+          <span className="text-xs sm:text-md p-1 rounded kbd border-primary text-primary">
             Web Developer | Backend
           </span>
         </h4>
+      </section>
+
+      {/* Summary */}
+      <section className="w-full my-2">
         <h2 className="my-2 text-lg sm:text-2xl font-semibold flex items-center gap-2">
-          <span>Summary</span> <BiPen />
+          <BiPen size={32} /> <span>Summary</span>
         </h2>
-        <p className="p-2">
-          Passionate and driven junior with a foundation in web
-          development, including skills in PHP, MySQL, HTML/CSS,
-          JavaScript, ReactJS, NodeJS, and RESTful API{"'"}s. Seeking an opportunity
-          to join a collaborative development team, where I can contribute my
-          skills and actively participate in the growth and success within your
-          organization.
+        <p className="mx-2">
+          Passionate and driven junior with a foundation in web development,
+          including skills in PHP, MySQL, HTML/CSS, JavaScript, ReactJS, NodeJS,
+          and RESTful API{"'"}s. Seeking an opportunity to join a collaborative
+          development team, where I can contribute my skills and actively
+          participate in the growth and success within your organization.
         </p>
       </section>
 
-
-      <section className="w-full pt-2">
-        <h2 className="my-2 text-lg sm:text-2xl font-semibold flex items-center gap-2">
-          <span>Work Experience</span> <FaPeopleGroup />
+      {/* Experience */}
+      <section className="w-full my-2">
+        <h2 className="my-3 text-lg sm:text-2xl font-semibold flex items-center gap-2">
+          <FaPeopleRoof size={32} />
+          <span>Experience | Internships</span>
         </h2>
-        <div>
-          <div className="collapse collapse-plus">
-            <input type="checkbox" name="" id="" />
-            <div className=" collapse-title">
-              <h2 >Zamboanga del Sur Electric Cooperative II</h2>
-              <a href="#" className="link-primary">Visit WebSite</a>
+        <div className="" id="company_list">
+          <div className="mt-2">
+            <div className="flex items-center justify-between">
+              <h3 className="font-bold flex gap-2 items-center">
+                <span className="">ZAMSURECO II</span>
+                <span className="text-slate-500 italic font-light">
+                  Pangi, Ipil, Zamboanga Sibugay 7001
+                </span>
+              </h3>
+              <h3 className="font-normal">2023 January - May</h3>
             </div>
-            <div className="collapse-content">
-              <h2 className="my-1 font-medium">Company</h2>
-              <ul className="mx-4">
-                <li className="font-bold">ZAMSURECO II</li>
-                <li className="">Pangi, Ipil , Zamboanga Sibugay</li>
-                <li className="italic">Student Intern | OJT</li>
-                <li className="font-medium">Warehouse Man</li>
-                <li className="font-semibold">2023 | January - May</li>
-              </ul>
-              <h2 className="my-1 font-medium">Skills Developed/Gained</h2>
-              <ul className="mx-4">
-                <li>Adaptability & Flexibility</li>
-                <li>Time Management</li>
-                <li>Task Management</li>
-                <li>Inventory</li>
-              </ul>
-            </div>
+            <div className="divider my-1"></div>
+            <ul className="mx-2">
+              <li className="font-medium">
+                Zamboanga del Sur Electric Cooprative II
+              </li>
+              <li className="">Engineering Department -- Warehouse Office</li>
+              <li className="">Student Intern | OJT -- Warehouse Man</li>
+            </ul>
           </div>
         </div>
-
       </section>
 
-      <section className="w-full pt-2">
-        <h2 className="my-2 text-lg sm:text-2xl font-semibold flex items-center gap-2">
-          <span>Education</span> <FaSchool />
+      {/* Education */}
+      <section className="w-full my-2">
+        <h2 className="my-3 text-lg sm:text-2xl font-semibold flex items-center gap-2">
+          <FaSchool size={32} />
+          <span>Education</span>
         </h2>
-        <ul className="mx-3 mt-1">
-          <li className="font-bold"> {'>'} Dr. Aurelio Mendoza Memorial Colleges</li>
-          <li className="italic">College of Computer Studies Department</li>
-          <li className="font-medium">BS in Information Technology</li>
-          <li className="font-semibold">2019 - 2023</li>
-        </ul>
-        <ul className="mx-3 mt-1">
-          <li className="font-bold">{'>'} Dr. Aurelio Mendoza Memorial Colleges</li>
-          <li className="italic">Technology, Vocational, & Livelihood Track (TVL)</li>
-          <li className="font-medium">Computer Programming</li>
-          <li className="font-semibold">2017 - 2019</li>
-        </ul>
+        <div className="" id="company_list">
+          <div className="mt-4">
+            <div className="flex items-center justify-between">
+              <h3 className="font-bold flex gap-2 items-center">
+                <span className="">Dr. Aurelio Mendoza Memorial Colleges</span>
+                <span className="text-slate-500 italic font-light">
+                  Poblacion, Ipil, Zamboanga Sibugay 7001
+                </span>
+              </h3>
+              <h3 className="font-normal">2019 - 2023</h3>
+            </div>
+            <div className="divider my-1"></div>
+            <ul className="mx-2">
+              <li className="font-medium">College of Computer Studies</li>
+              <li className="">
+                Bachelor of Science in Information Technology
+              </li>
+            </ul>
+          </div>
+          <div className="mt-4">
+            <div className="flex items-center justify-between">
+              <h3 className="font-bold flex gap-2 items-center">
+                <span className="">Dr. Aurelio Mendoza Memorial Colleges</span>
+                <span className="text-slate-500 italic font-light">
+                  Poblacion, Ipil, Zamboanga Sibugay 7001
+                </span>
+              </h3>
+              <h3 className="font-normal">2017 - 2019</h3>
+            </div>
+            <div className="divider my-1"></div>
+            <ul className="mx-2">
+              <li className="font-medium">
+                Technology, Vocational & Livelihood Track (TVL)
+              </li>
+              <li className="">Computer Programming</li>
+            </ul>
+          </div>
+        </div>
       </section>
 
-      {/* Skills & Tech */}
-      <section className="w-full pt-2">
+      {/* Personal Skills */}
+      <section className="w-full my-2">
         <h2 className="my-2 text-lg sm:text-2xl font-semibold flex items-center gap-2">
-          <span className="">Skills & Tech</span> <FaGear />
+          <FaPerson size={32} />
+          <span>Personal Skills</span>
+        </h2>
+        {personal_skills.map((skill, skillIndex) => {
+          return (
+            <ul key={skillIndex} className="my-2 mx-3">
+              <li className="flex items-center gap-2 hover: cursor-default hover:bg-primary hover:text-white p-1">
+                <FaArrowRight /> {skill.name}
+              </li>
+            </ul>
+          );
+        })}
+      </section>
+
+      {/* Programming/Development Skills */}
+      <section className="w-full my-2">
+        <h2 className="my-2 text-lg sm:text-2xl font-semibold flex items-center gap-2">
+          <FaGear size={32} />{" "}
+          <span className="">Programming/Development Skills</span>
         </h2>
         {/* Data Here */}
 
         {skills.map((skill, skillIndex) => {
-          return <div className="p-4" key={skillIndex}>
-            <h3 className="text-xl font-bold">{skill.name}</h3>
-            <div className="my-2">
-              <h4 className="ml-2 my-2 text-lg font-medium">Languages</h4>
-              <ul className="ml-4 flex gap-2 items-center flex-wrap">
-                {skill.tech.lang.map((stack, stackIndex) => {
-                  return <li key={stackIndex} className="tooltip tooltip-primary" data-tip={stack.name}>
-                    {stack.icon}
-                  </li>
-                })}
-              </ul>
+          return (
+            <div className="p-4" key={skillIndex}>
+              <h3 className="text-xl font-bold">{skill.name}</h3>
+              <div className="my-2">
+                <h4 className="ml-2 my-2 text-lg font-medium">Languages</h4>
+                <ul className="ml-4 flex gap-2 items-center flex-wrap">
+                  {skill.tech.lang.map((stack, stackIndex) => {
+                    return (
+                      <li
+                        key={stackIndex}
+                        className="tooltip tooltip-primary"
+                        data-tip={stack.name}
+                      >
+                        {stack.icon}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+              <div className="my-2">
+                <h4 className="ml-2 my-2 text-lg font-medium">Frontend - UI</h4>
+                <ul className="ml-4 flex gap-2 items-center flex-wrap">
+                  {skill.tech.front_stack.map((stack, stackIndex) => {
+                    return (
+                      <li
+                        key={stackIndex}
+                        className="tooltip tooltip-primary"
+                        data-tip={stack.name}
+                      >
+                        {stack.icon}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+              <div className="my-2">
+                <h4 className="ml-2 my-2 text-lg font-medium">
+                  Backend - REST API
+                </h4>
+                <ul className="ml-4 py-2 flex gap-2 items-center flex-wrap  ">
+                  {skill.tech.back_stack.map((stack, stackIndex) => {
+                    return (
+                      <li
+                        key={stackIndex}
+                        className="tooltip tooltip-primary"
+                        data-tip={stack.name}
+                      >
+                        {stack.icon}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
             </div>
-            <div className="my-2">
-              <h4 className="ml-2 my-2 text-lg font-medium">Frontend - UI</h4>
-              <ul className="ml-4 flex gap-2 items-center flex-wrap">
-                {skill.tech.front_stack.map((stack, stackIndex) => {
-                  return <li key={stackIndex} className="tooltip tooltip-primary" data-tip={stack.name}>
-                    {stack.icon}
-                  </li>
-                })}
-              </ul>
-            </div>
-            <div className="my-2">
-              <h4 className="ml-2 my-2 text-lg font-medium">Backend - REST API</h4>
-              <ul className="ml-4 py-2 flex gap-2 items-center flex-wrap  ">
-                {skill.tech.back_stack.map((stack, stackIndex) => {
-                  return <li key={stackIndex} className="tooltip tooltip-primary" data-tip={stack.name}>
-                    {stack.icon}
-                  </li>
-                })}
-              </ul>
-            </div>
-          </div>
+          );
         })}
-      </section >
-      {/* <!-- //TODO --> Contact */}
-      < section className="w-full  pt-2" >
-        <h2 className="my-2 text-lg sm:text-2xl font-semibold flex justify-start items-center gap-1">
-          Reach Me Now @
+      </section>
+      {/*  Contact */}
+      <section className="w-full  my-2">
+        <h2 className="my-2 text-lg sm:text-2xl font-semibold flex justify-start items-center gap-2">
+          <FaMailBulk size={32} />
+          <span>Contact</span>
         </h2>
         {/* Data Here */}
-        {
-          info.map((infoItem, infoIndex) => {
-            return <div className=" relative group overflow-hidden" key={infoIndex}>
+        {info.map((infoItem, infoIndex) => {
+          return (
+            <div className=" relative group overflow-hidden" key={infoIndex}>
               <ul className=" cursor-default flex items-center gap-3 relative">
                 <li className="my-1 relative overflow-hidden">
-                  <p className="peer group-hover:text-sky-600">{infoItem?.icon}</p>
+                  <p className="peer group-hover:text-sky-600">
+                    {infoItem?.icon}
+                  </p>
                 </li>
-                <li className="my-1 group-hover:text-sky-600">{infoItem.name}</li>
-                <li onClick={() => {
-                  navigator.clipboard.writeText(infoItem.content)
-                }} className="my-1 cursor-pointer p-2 hover:bg-sky-600 hover:text-black">{infoItem.content}</li>
+                <li className="my-1 group-hover:text-sky-600">
+                  {infoItem.name}
+                </li>
+                <li
+                  onClick={() => {
+                    navigator.clipboard.writeText(infoItem.content);
+                  }}
+                  className="my-1 cursor-pointer p-2 hover:bg-sky-600 hover:text-black"
+                >
+                  {infoItem.content}
+                </li>
               </ul>
               <div className="p-[2px] bg-sky-600 w-0 absolute bottom-0 -left-full group-hover:left-0 group-hover:w-full duration-500"></div>
             </div>
-          })
-        }
-      </ section>
+          );
+        })}
+      </section>
 
-
-      <section className="w-full pt-2">
+      {/* Projects/Hobbies */}
+      <section className="w-full my-2">
         <h2 className="my-2 text-lg sm:text-2xl font-semibold flex items-center gap-2">
-          <span>Simple Hobby Projects</span> <FaDiagramProject />
+          <FaDiagramProject size={32} />
+          <span>Simple Hobby Projects</span>
         </h2>
         <p className=" pl-3 flex items-center flex-wrap gap-2">
           <span className="flex items-center gap-2">
@@ -178,7 +249,8 @@ export default function Home() {
             <a
               className="flex items-center gap-1 p-1 rounded bg-black text-white"
               href="https://github.com/Roald2000"
-              target="_blank" rel="noreferrer"
+              target="_blank"
+              rel="noreferrer"
             >
               github <FaGithub />
             </a>{" "}
@@ -186,7 +258,6 @@ export default function Home() {
           </span>
         </p>
       </section>
-
     </>
   );
 }
